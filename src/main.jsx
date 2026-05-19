8cedd59999cd23ccbf9dbd2bbb77ed4f2d3c3cd3
Adil <rencakaaa13@gmail.com>
2026-05-19 15:28:02 +0700

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+
+/* VERCEL ANALYTICS */
+import { Analytics } from "@vercel/analytics/react";
+
+/* VERCEL SPEED INSIGHTS */
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+
+    {/* Analytics Pengunjung */}
+    <Analytics />
+
+    {/* Speed & Performance */}
+    <SpeedInsights />
+
+  </React.StrictMode>
 )
